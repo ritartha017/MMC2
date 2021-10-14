b500bd61a701cdd92b4b21d0e55e327fb02e1ef7
ritartha017 <ritartha017@bk.ru>
2021-10-14 22:19:56 +0300

    refactor F-R beta func

--- 2_conjugate_gradient_var2.rb
+++ 2_conjugate_gradient_var2.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 @a = 4
 @b = 3
 
@@ -19,7 +21,7 @@ def fetch_alpha(gk, dk)
     dk[0] * Q[0] + dk[1] * Q[1],
     dk[0] * Q[2] + dk[1] * Q[3]
   ]
-  res = gk_dk / (dk_q[0] * dk[0] + dk_q[1] * dk[1]).to_f
+  res = gk_dk / (dk_q[0] * dk[0] + dk_q[1] * dk[1]).to_f.send(:*, -1)
 end
 
 def fetch_x(xk, alphak, dk)
@@ -27,12 +29,13 @@ def fetch_x(xk, alphak, dk)
    xk[1] + alphak * dk[1]]
 end
 
-def get_hestenes_stiefel_beta(magnk, magnk1)
-  res = (magnk1**2).abs / (magnk**2).abs.to_f
+def get_hestenes_stiefel_beta(magnitude_k, magnitude_k1)
+  res = (magnitude_k1**2).abs / (magnitude_k**2).abs.to_f
 end
 
-def get_fletcher_reeves_beta(gk, gk1)
-  (gk1[0]*gk1[0] + gk1[1]*gk1[1])/(gk[0]*gk[0] + gk[1]*gk[1]).to_f
+def get_fletcher_reeves_beta(xk, gk1)
+  gk = fetch_partial_derivatives(xk[0], xk[1])
+  (gk1[0] * gk1[0] + gk1[1] * gk1[1]) / (gk[0] * gk[0] + gk[1] * gk[1]).to_f
 end
 
 def fetch_dk1(gk1, beta, dk)
@@ -49,21 +52,22 @@ def minimize(a, b)
   xk = [a, b]
   gk1 = fetch_partial_derivatives(xk[0], xk[1])
   dk1 = [-gk1[0], -gk1[1]]
-  magnk = get_magnitude(gk1[0], gk1[1])
-  puts "g0= #{gk1}\n" + "mk0= #{magnk}"
+  magnitude_k = get_magnitude(gk1[0], gk1[1])
+  puts "g0= #{gk1}\n" + "mk0= #{magnitude_k}"
   loop do
     k += 1
-    alphak = -fetch_alpha(gk1, dk1)
+    alphak = fetch_alpha(gk1, dk1)
     xk1 = fetch_x(xk, alphak, dk1)
     fxk = substitute_into_fxy(xk1[0], xk1[1])
     gk1 = fetch_partial_derivatives(xk1[0], xk1[1])
-    magnk1 = get_magnitude(gk1[0], gk1[1])
-    # beta = get_hestenes_stiefel_beta(magnk, magnk1)
-    beta = get_fletcher_reeves_beta(fetch_partial_derivatives(xk[0], xk[1]), gk1)
+    magnitude_k1 = get_magnitude(gk1[0], gk1[1])
+    # beta = get_hestenes_stiefel_beta(magnitude_k, magnitude_k1)
+    beta = get_fletcher_reeves_beta(xk, gk1)
     dk1 = fetch_dk1(gk1, beta, dk1)
-    puts "k= #{k} \n"    + "alpha= #{alphak}\n"+ "x = #{xk1} \n" + "g = #{gk1} \n" \
-         "B = #{beta}\n"   + "Fx= #{fxk}" + "\nM = #{magnk1}" +"\nd = #{dk1}\n\n"
-    xk, magnk = xk1, magnk1
+    puts "k= #{k} \n" + "alpha= #{alphak}\n" + "x = #{xk1} \n" + "g = #{gk1} \n" \
+         "B = #{beta}\n" + "Fx= #{fxk}" + "\nM = #{magnitude_k1}" + "\nd = #{dk1}\n\n"
+    xk = xk1
+    magnitude_k = magnitude_k1
     return xk if (gk1[0]).zero? && (gk1[1]).zero?
   end
 end
